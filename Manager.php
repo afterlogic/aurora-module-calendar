1e1bb367b2daf890e4890f704d04614b40a339d9
vasil <vasil.sokolov@gmail.com>
2017-07-28 17:59:53 +0300

    updated license information

--- Manager.php
+++ Manager.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * @copyright Copyright (c) 2017, Afterlogic Corp.
- * @license AGPL-3.0 or AfterLogic Software License
+ * @license AfterLogic Software License
  *
- * This code is licensed under AGPLv3 license or AfterLogic Software License
- * if commercial version of the product was purchased.
- * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ * This code is licensed under AfterLogic Software License.
+ * For full statements of the license see LICENSE file.
  */
 
 namespace Aurora\Modules\Calendar;
--- classes/enum.php
+++ classes/enum.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AfterLogic Software License
+ *
+ * This code is licensed under AfterLogic Software License.
+ * For full statements of the license see LICENSE file.
+ */
+
+/**
  * @package Api
  * @subpackage Enum
  */
