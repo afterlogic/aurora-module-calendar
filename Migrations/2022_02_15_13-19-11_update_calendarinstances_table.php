bfdad955a0087c8b08f8508a910467e8dbf62396
sash04ek <a.ovcharov@afterlogic.com>
2022-02-15 16:47:05 +0300

    removed Encoding lib

--- Migrations/2022_02_15_13-19-11_update_calendarinstances_table.php
+++ Migrations/2022_02_15_13-19-11_update_calendarinstances_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Capsule\Manager as Capsule;
-use Aurora\System\Classes\Encoding;
 
 class UpdateCalendarinstancesTable extends Migration
 {
