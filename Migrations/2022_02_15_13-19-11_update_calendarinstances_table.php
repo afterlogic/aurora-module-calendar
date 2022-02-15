7a21d5a60b77ecf70d12d535c9a717ae4c139d4f
sash04ek <a.ovcharov@afterlogic.com>
2022-02-15 13:21:45 +0300

    fixed issue Encoding-related errors on Create/Update Tables

--- Migrations/2022_02_15_13-19-11_update_calendarinstances_table.php
+++ Migrations/2022_02_15_13-19-11_update_calendarinstances_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Aurora\System\Classes\Encoding;
 
 class UpdateCalendarinstancesTable extends Migration
 {
@@ -14,7 +15,10 @@ class UpdateCalendarinstancesTable extends Migration
     public function up()
     {
         $prefix = Capsule::connection()->getTablePrefix();
-        Capsule::statement("UPDATE {$prefix}adav_calendarinstances SET displayname = CONVERT(cast(CONVERT(displayname USING latin1) AS BINARY) USING utf8)");
+        Capsule::statement("SET NAMES latin1");
+        Capsule::statement("UPDATE {$prefix}adav_calendarinstances SET displayname = CONVERT(cast(CONVERT(displayname USING latin1) AS BINARY) USING utf8)
+        WHERE CONVERT(displayname USING latin1) = CONVERT(displayname USING utf8) AND LENGTH(displayname) != CHAR_LENGTH(displayname)");
+        Capsule::statement("SET NAMES utf8");
     }
 
     /**
