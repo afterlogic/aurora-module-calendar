210403e983dce814196087af353462dfd91b2f89
sash04ek <a.ovcharov@gmail.com>
2025-08-19 16:48:07 +0300

    removed debug time

--- Cron.php
+++ Cron.php
@@ -491,7 +491,7 @@ class Reminder
         \Aurora\System\Api::Log('---------- Start cron script', \Aurora\System\Enums\LogLevel::Full, 'cron-');
 
         $oTimeZoneUTC = new \DateTimeZone('UTC');
-        $oNowDT_UTC = new \DateTimeImmutable('19.08.2025 13:10:00', $oTimeZoneUTC);
+        $oNowDT_UTC = new \DateTimeImmutable('now', $oTimeZoneUTC);
 
         $iNowTS = $oNowDT_UTC->getTimestamp();
 
